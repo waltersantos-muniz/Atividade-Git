9114a8e574ef937fd1ceaf65d95ece2b0a5b3b86
waltersantos-muniz <walter.ms.santos41@gmail.com>
2019-08-08 12:16:07 -0300

    inicio do projeto clonado

--- Acervo-master/src/acervo/livro.java
+++ Acervo-master/src/acervo/livro.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package acervo;
 
-/**
- *
- * @author Aluno01
- */
 public class livro {
+    String titulo;
+    String autor;
+    String isbn;
+    String editora;
+    int qtd_pag;
+    int ano;
+    double valor;
     
-}
+    }
